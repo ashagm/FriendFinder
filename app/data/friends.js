105f6af885aecb43843af5fe0dd3596ecf611ca5
asha <ashaagm@gmail.com>
2018-02-28 13:14:59 -0500

    routes and some html

---
+++ app/data/friends.js
@@ -0,0 +1,68 @@
+var friends = [
+{
+    "name": "Brad",
+    "photo": "http://cdn28.us3.fansshare.com/photograph/bradpitt/brad-pitt-fashion-166284654.jpg",
+    "scores": [
+        5,
+        1,
+        2,
+        5,
+        1,
+        4,
+        5,
+        1,
+        4,
+        3
+    ]
+}, 
+{
+    "name": "Matt",
+    "photo": "http://cdn3.uvnimg.com/dc/85/282a3a904612b3e688467b870905/matt-damon.jpg",
+    "scores": [
+        2,
+        5,
+        2,
+        4,
+        3,
+        1,
+        4,
+        2,
+        5,
+        5
+    ]
+},
+{
+    "name": "Daniel",
+    "photo": "http://cdn01.cdn.justjared.com/wp-content/uploads/headlines/2015/09/dan-mag.jpg",
+    "scores": [
+        3,
+        4,
+        2,
+        1,
+        2,
+        1,
+        5,
+        2,
+        4,
+        5
+    ]
+},
+{
+    "name": "Harrison",
+    "photo": "http://cdn.stylisheve.com/wp-content/uploads/2012/01/Hairstyles-for-over-50-men_17.jpg",
+    "scores": [
+        1,
+        2,
+        3,
+        4,
+        5,
+        4,
+        3,
+        5,
+        3,
+        3
+    ]
+}
+];
+
+module.exports = friends;
